33a05ad3be12525b812c234d162aa858ad26282d
Darren Torpey <dct@punchbowlsoftware.com>
2009-11-23 07:11:16 -0500

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key    => "_CHANGEME_session",
-  :secret => "CHANGESESSION"
+  :key    => "_coledge_session",
+  :secret => "c1d7567cf548873048295dcc81585400"
 }
 
 # Use the database for sessions instead of the cookie-based default,
