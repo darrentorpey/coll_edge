e1b22113460a8a755822aa7e49621707302b41c1
Darren Torpey <dct@punchbowlsoftware.com>
2010-01-24 20:43:20 -0500

    Setup some basic CSS

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,3 @@
-# Be sure to restart your server when you modify this file
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
@@ -65,4 +63,3 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
 end
-
