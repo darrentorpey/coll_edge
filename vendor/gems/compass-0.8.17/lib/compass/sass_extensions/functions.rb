18387b4a34dcf494b5afa83e3383dadf6b5f4780
Darren Torpey <dct@punchbowlsoftware.com>
2010-01-25 21:15:13 -0500

    More Sassy goodness!

--- vendor/gems/compass-0.8.17/lib/compass/sass_extensions/functions.rb
+++ vendor/gems/compass-0.8.17/lib/compass/sass_extensions/functions.rb
@@ -1,6 +1,21 @@
 module Compass::SassExtensions::Functions
 end
 
+module Sass::Script::Functions
+  def reverse(string)
+    assert_type string, :String
+    Sass::Script::String.new(string.value.reverse)
+  end
+
+  def blend(one_color, other_color, percent = Sass::Script::Number.new(50))
+    assert_type one_color, :Color
+    assert_type other_color, :Color
+    assert_type percent, :Number
+    blended = (one_color.div(Sass::Script::Number.new(100).div(percent))).plus((other_color).div(Sass::Script::Number.new(100).div(Sass::Script::Number.new(100).minus(percent))))
+    Sass::Script::Color.new(blended.value)
+  end
+end
+
 ['selectors', 'enumerate', 'urls', 'display', 'inline_image'].each do |func|
   require File.join(File.dirname(__FILE__), 'functions', func)
 end
@@ -17,4 +32,3 @@ end
 class Sass::Script::Functions::EvaluationContext
   include Sass::Script::Functions
 end
-
